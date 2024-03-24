f6cbee8f9356184554b602cdc590cc0472ba3bba
Pedro Fonseca <97555978+Pedro-GFonseca@users.noreply.github.com>
2024-03-24 16:49:12 -0300

    Update music_messages.py

--- music_messages.py
+++ music_messages.py
@@ -1,4 +1,3 @@
-join_message = ["ERROR 42069: TOO MUCH BOIOLAGEM FOR MY LINKING", "cheguei porra", "\"Um homem de pau duro é uma fera que caminha\" - Hermes e Renato.", 
-"vc foi no passeio?", "com calma e jeito chega-se ao cu de qualquer sujeito", "passou de dois já é suruba", "vc conhece a kelly?"]
-leave_message = ["tô vazando", "\"O meu pau é maior, I\'m sorry\" - Apple, Jupyter.", "coach e psicólogo é a mesma coisa", "pino de pipicles", "cuzin com limão", "n fico em channel com autista", "vamo usar droga"]
-login_message = ["O pai tá on"]
+join_message = ["INSERT YOUR MESSAGES HERE"] # You should adjust the amount of messages in the music_cog.py based on the amount of messages you introduce here
+leave_message = ["INSERT YOUR MESSAGES HERE"] # You should adjust the amount of messages in the music_cog.py based on the amount of messages you introduce here
+login_message = ["INSERT YOUR MESSAGE HERE"] # This should be a list with a single message in it
